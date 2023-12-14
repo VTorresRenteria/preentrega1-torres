11d44b578d62084157a22c8c2f57f4f64e638f35
Victor Torres <fernando011002@gmail.com>
2023-12-14 02:45:46 -0600

    Se agrega para que el usuario solo ingrese numeros

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 function promedio(personas, edadAcumulada) {
-    return edadAcumulada / personas;
+    let resultado = edadAcumulada / personas
+    return resultado
+}
+
+let personas;
+
+while (true) {
+    personas = Number(prompt("Ingresa la cantidad de personas"));
+
+    if (!isNaN(personas) && personas > 0) {
+        break;
+    } else {
+        alert("Número inválido de personas.");
+    }
 }
 
 let edadAcumulada = 0;
 
-let personas = Number(prompt("Ingresa la cantidad de personas para calcular edad"));
+for (let i = 1; i <= personas; i++) {
+    let edad;
+    while (true) {
+        edad = Number(prompt(`Ingresa la edad de la persona ${i}`));
 
-if (personas > 0) {
-
-    for (let i = 1; i <= personas; i++) {
-        let edad = Number(prompt(`ingresa la edad de la persona ${i}`));
-        edadAcumulada = edadAcumulada + edad;
+        if (!isNaN(edad)) {
+            edadAcumulada += edad;
+            break;
+        } else {
+            alert("Edad inválida.");
+        }
     }
-
-    let resultado = promedio(personas, edadAcumulada);
-
-    alert(`El promedio de edad es ${resultado}`);
-
-} else{
-    alert("numero invalido de personas")
 }
+
+let promedioEdad = promedio(personas, edadAcumulada);
+promedioEdad = Math.round(promedioEdad);
+alert(`El promedio de edad es ${promedioEdad} años`);
+
